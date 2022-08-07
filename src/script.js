14a071c85a86e815bd1024de5eadfd0b21a58f12
Egor <georgebazhenov27@gmail.com>
2022-08-07 14:46:36 +0300

    place

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
 /**
@@ -8,15 +9,17 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
  */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-const container = document.createElement( 'div' );
+const container = document.createElement( 'div' )
+
+document.addEventListener("keydown", onDocumentKeyDowne, false);
 document.body.appendChild( container );
 
 // Scene
 const scene = new THREE.Scene()
 scene.background = new THREE.Color( 0xa0a0a0 );
-scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+scene.fog = new THREE.Fog( 0xa0a0a0, 300, 1000 );
 
-let mixer, renderer, stats
+let mixer, renderer
 
 /**
  * Sizes
@@ -45,8 +48,8 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-camera.position.set( 100, 200, 300 );
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 2000 );
+camera.position.set( 300, 300, 300 );
 
 
 //light
@@ -69,31 +72,34 @@ mesh.rotation.x = - Math.PI / 2;
 mesh.receiveShadow = true;
 scene.add( mesh );
 
-const grid = new THREE.GridHelper( 2000, 10, 0x000000, 0x000000 );
+const grid = new THREE.GridHelper( 2000, 40, 0x000000, 0x000000 );
 grid.material.opacity = 0.2;
 grid.material.transparent = true;
 scene.add( grid );
 
+//place
+
+let place
+const loader = new GLTFLoader()
+loader.load('destroy/scene.gltf', (objPlace) => {
+	objPlace.scene.scale.set(100,100,100)
+    objPlace.scene.rotation.y = 0.45
+    objPlace.scene.position.y = -230
+    //objPlace.scene.position.y = 550
+    scene.add(objPlace.scene)
+})
+
 // model
-const loader = new FBXLoader();
-loader.load( 'Walking.fbx', function ( object ) {
+let bot
+const StandLoader = new FBXLoader();
+StandLoader.load( 'Standing.fbx', function ( fbxBot ) {
 
-    mixer = new THREE.AnimationMixer( object );
+    mixer = new THREE.AnimationMixer( fbxBot );
     
-
-    const action = mixer.clipAction( object.animations[ 0 ] );
+    const action = mixer.clipAction( fbxBot.animations[ 0 ] );
     action.play();
 
-    object.traverse( function ( child ) {
-
-        if ( child.isMesh ) {
-
-            child.castShadow = true;
-            child.receiveShadow = true;
-
-        }
-    } );
-    scene.add( object );
+    scene.add( fbxBot );
 } );
 
 
@@ -107,17 +113,43 @@ const controls = new OrbitControls( camera, renderer.domElement );
 controls.target.set( 0, 100, 0 );
 controls.update();
 
+function onDocumentKeyDowne(e) {
+	if (e.keyCode === 38) {
+		
+    const walkLoader = new FBXLoader();
+    walkLoader.load( 'Walking.fbx', function ( walkBot ) {
+
+    mixer = new THREE.AnimationMixer( walkBot );
+    
+    const action = mixer.clipAction( walkBot.animations[ 0 ] );
+    action.play();
+
+    walkBot.traverse( function ( child ) {
+        if ( child.isMesh ) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    } );
+    scene.add( walkBot );
+} );
+	} else if(e.keyCode === 39) {
+        grid.rotation.y += 0.01	
+	}
+}
+
+
 /**
  * Animate
  */
 const clock = new THREE.Clock()
-let lastElapsedTime = 0
 
 function animate() {
 
     requestAnimationFrame( animate );
 
     const delta = clock.getDelta();
+
+    //grid.position.z -= 0.2
 
     if ( mixer ) mixer.update( delta );
 
