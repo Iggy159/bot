d4120efe1087c848a193d9bbcd46e4044da56d02
Egor <georgebazhenov27@gmail.com>
2022-09-17 19:30:28 +0300

    macarena

--- src/script.js
+++ src/script.js
@@ -1,23 +1,28 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+import { ShaderPass } from "./../node_modules/three/examples/jsm/postprocessing/ShaderPass";
+
+import { EffectComposer } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "/node_modules/three/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import { RGBShiftShader } from "./../node_modules/three/examples/jsm/shaders/RGBShiftShader";
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+
 const container = document.createElement( 'div' )
 
-document.addEventListener("keydown", onDocumentKeyDowne, false);
 document.body.appendChild( container );
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color( 0xa0a0a0 );
-scene.fog = new THREE.Fog( 0xa0a0a0, 300, 1000 );
+scene.background = new THREE.Color( 0x000000 );
+scene.fog = new THREE.Fog( 0x000000, 300, 1000 );
 
 let mixer, renderer
 
@@ -48,26 +53,25 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 2000 );
-camera.position.set( 300, 300, 300 );
+const camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 2000 );
+camera.position.set( 0, 100, 300 );
 
 
 //light
-const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-hemiLight.position.set( 0, 200, 0 );
+const hemiLight = new THREE.HemisphereLight( 0xff00b3, 0x444444 );
+hemiLight.position.set( 100, 200, 0 );
 scene.add( hemiLight );
 
-const dirLight = new THREE.DirectionalLight( 0xffffff );
-dirLight.position.set( 0, 200, 100 );
+const dirLight = new THREE.DirectionalLight( 0xff00b3 );
+dirLight.position.set( 0, 200, 300 );
 dirLight.castShadow = true;
 dirLight.shadow.camera.top = 180;
 dirLight.shadow.camera.bottom = - 100;
-dirLight.shadow.camera.left = - 120;
-dirLight.shadow.camera.right = 120;
+
 scene.add( dirLight );
 
 // ground
-const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 4000, 4000 ), new THREE.MeshPhongMaterial( { color: 0x000538, depthWrite: false } ) );
 mesh.rotation.x = - Math.PI / 2;
 mesh.receiveShadow = true;
 scene.add( mesh );
@@ -75,24 +79,12 @@ scene.add( mesh );
 const grid = new THREE.GridHelper( 2000, 40, 0x000000, 0x000000 );
 grid.material.opacity = 0.2;
 grid.material.transparent = true;
-scene.add( grid );
-
-//place
-
-let place
-const loader = new GLTFLoader()
-loader.load('destroy/scene.gltf', (objPlace) => {
-	objPlace.scene.scale.set(100,100,100)
-    objPlace.scene.rotation.y = 0.45
-    objPlace.scene.position.y = -230
-    //objPlace.scene.position.y = 550
-    scene.add(objPlace.scene)
-})
+scene.add( grid )
 
 // model
 let bot
 const StandLoader = new FBXLoader();
-StandLoader.load( 'Standing.fbx', function ( fbxBot ) {
+StandLoader.load( 'Macarena.fbx', function ( fbxBot ) {
 
     mixer = new THREE.AnimationMixer( fbxBot );
     
@@ -113,31 +105,26 @@ const controls = new OrbitControls( camera, renderer.domElement );
 controls.target.set( 0, 100, 0 );
 controls.update();
 
-function onDocumentKeyDowne(e) {
-	if (e.keyCode === 38) {
-		
-    const walkLoader = new FBXLoader();
-    walkLoader.load( 'Walking.fbx', function ( walkBot ) {
+const renderScene = new RenderPass( scene, camera );
 
-    mixer = new THREE.AnimationMixer( walkBot );
-    
-    const action = mixer.clipAction( walkBot.animations[ 0 ] );
-    action.play();
-
-    walkBot.traverse( function ( child ) {
-        if ( child.isMesh ) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        }
-    } );
-    scene.add( walkBot );
-} );
-	} else if(e.keyCode === 39) {
-        grid.rotation.y += 0.01	
-	}
-}
+const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+bloomPass.exposure = 0.0
+bloomPass.threshold = 0.2;
+bloomPass.strength = 1.5; //intensity of glow
+bloomPass.radius = 0.8;
 
 
+const effect2 = new ShaderPass( RGBShiftShader );
+effect2.uniforms[ 'amount' ].value = 0.0025;
+
+
+const composer = new EffectComposer( renderer );
+composer.addPass( bloomPass );
+composer.setSize(window.innerWidth, window.innerHeight); 
+composer.renderToScreen = true; 
+composer.addPass(renderScene); 
+composer.addPass(bloomPass)
+composer.addPass( effect2 );
 /**
  * Animate
  */
@@ -149,11 +136,10 @@ function animate() {
 
     const delta = clock.getDelta();
 
-    //grid.position.z -= 0.2
-
     if ( mixer ) mixer.update( delta );
 
-    renderer.render( scene, camera );
+    composer.render( scene, camera );
+
 }
 
 animate()
